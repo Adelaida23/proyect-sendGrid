85da58e6d406b3c1fd3fb553d7747eaba2848e08
chessed03 <chessed03@gmail.com>
2022-06-29 10:14:54 -0500

    add sender elastic mail

--- services/MailSender/SenderElasticEmail.php
+++ services/MailSender/SenderElasticEmail.php
@@ -14,8 +14,8 @@ class SenderElasticEmail extends MailSender
     public function __construct()
     {
         $this->init();
-        $this->apiKey     = env('API_KEY_MAILGUN');
-        $this->domain     = env('DOMAIN_NAME_MAILGUN');
+        $this->apiKey     = env('API_KEY_ELASTICEMAIL');
+        $this->domain     = env('DOMAIN_NAME_ELASTICEMAIL');
     }
 
     public function listEmails( array $listEmails )
@@ -25,41 +25,40 @@ class SenderElasticEmail extends MailSender
 
     public function send()
     {
-
-        $url = 'https://api.elasticemail.com/v2/email/send';
-
         try{
-            $post = array('from' => $this->fromEmail,
-                'fromName' => $this->fromName,
-                'apikey' => '657DB740199661DFE08AFBD6107B5BA184C6AB4E8E3468C79CAAE1035FECE1275649648775268A7FE7729046C62FF3AF',
-                'subject' => $this->subject,
-                'to' => $this->listEmails,
-                'bodyHtml' => '<h1>Html Body</h1>',
-                'bodyText' => 'Text Body',
-                'isTransactional' => false);
+            $mailClient = array(
+                'from'            => $this->fromEmail,
+                'fromName'        => $this->fromName,
+                'apikey'          => $this->apiKey,
+                'subject'         => $this->subject,
+                'to'              => $this->listEmails,
+                'bodyHtml'        => $this->message,
+                'isTransactional' => false
+            );
 
             $ch = curl_init();
+
             curl_setopt_array($ch, array(
-                CURLOPT_URL => $url,
-                CURLOPT_POST => true,
-                CURLOPT_POSTFIELDS => $post,
+                CURLOPT_URL            => $this->domain,
+                CURLOPT_POST           => true,
+                CURLOPT_POSTFIELDS     => $mailClient,
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_HEADER => false,
+                CURLOPT_HEADER         => false,
                 CURLOPT_SSL_VERIFYPEER => false
             ));
 
             $result=curl_exec ($ch);
+
             curl_close ($ch);
-            dd($result);
-            echo $result;
+
+            return back()->with('success', "Send emails correctly \n");
         }
         catch(Exception $ex){
             echo $ex->getMessage();
+
+            return back()->with('error', "Error \n");
         }
 
     }
 
 }
-/* API KEY
- * 657DB740199661DFE08AFBD6107B5BA184C6AB4E8E3468C79CAAE1035FECE1275649648775268A7FE7729046C62FF3AF
- */
